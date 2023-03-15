960c48dacbd15d4ecd96752c38218814b213794a
Nishanth Kumar <nishanth.kumar20@gmail.com>
2023-03-14 23:31:12 -0400

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # lis-spot-utils
 A number of helpful utilities for doing cool things with the Boston Dynamics Spot Robot!
+
+## Installation
+(Note: Performing the installation in a separate virtualenv or conda environment is
+recommended!)
+Users should just be able to run `pip install -e .` 
+Developers should run `pip install -e .[develop]`
---
+++ setup.py
@@ -0,0 +1,20 @@
+"""Setup script."""
+from setuptools import find_packages, setup
+
+setup(
+    name="spot-utils",
+    version="0.1.0",
+    packages=[],  # TODO #find_packages(include=["predicators", "predicators.*"]),
+    install_requires=[],
+    include_package_data=True,
+    extras_require={
+        "develop": [
+            "black",
+            "docformatter",
+            "isort",
+            "mypy@git+https://github.com/python/mypy.git@9bd651758e8ea2494"
+            + "837814092af70f8d9e6f7a1",
+            "mypy_extensions",
+        ]
+    },
+)
