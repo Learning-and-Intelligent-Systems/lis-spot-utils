1bab5afe5a4680e5144120afe97701857c72b941
Tom Silver <tomssilver@gmail.com>
2023-03-15 17:23:55 -0400

    i guess the checks work...

--- spot_utils/hello_world.py
+++ spot_utils/hello_world.py
@@ -1,11 +1,9 @@
 """This is a temporary file that should be deleted once we merge some actual
 code."""
 
-from spot_utils.bullshit.please_ignore import spot_is_fun
 
 def main() -> None:
     """This is a temporary function that will be deleted soon."""
-    assert spot_is_fun()
     print("Hello, world!")
 
 
