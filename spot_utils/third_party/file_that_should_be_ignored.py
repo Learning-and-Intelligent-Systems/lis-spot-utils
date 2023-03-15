a43259131fc200413e452af4b9e83703999a0db0
Tom Silver <tomssilver@gmail.com>
2023-03-15 17:26:59 -0400

    remove ds store

---
+++ spot_utils/third_party/file_that_should_be_ignored.py
@@ -0,0 +1,8 @@
+"""This is a terrible file that should be totally totally totally totally totally totally totally ignored by all of the checks."""
+
+import numpy as np
+import itertools
+
+
+def f():
+    x = y
