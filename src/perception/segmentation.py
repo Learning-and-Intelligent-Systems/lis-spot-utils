9f4c866d8ec79ba84cc733879655bfb890011ee3
Aidan Curtis <curtisa@mit.edu>
2023-03-15 16:02:29 -0400

    ignore vision utils

--- src/perception/segmentation.py
+++ src/perception/segmentation.py
@@ -1,5 +1,5 @@
 from structures.image import RGBImage
 
 def scene_segment_image(rgbd:RGBImage) -> RGBImage:
-    from src.perception.semseg.interface import get_semantic_labels
+    from src.perception.vision_utils.semseg.interface import get_semantic_labels
     return get_semantic_labels(rgbd.rgb)
