269b9cd778bab431090cdf3e3c830df632d38991
Tom Silver <tomssilver@gmail.com>
2023-03-15 17:22:48 -0400

    make it possible to ignore third party repo

---
+++ spot_utils/hello_world.py
@@ -0,0 +1,13 @@
+"""This is a temporary file that should be deleted once we merge some actual
+code."""
+
+from spot_utils.bullshit.please_ignore import spot_is_fun
+
+def main() -> None:
+    """This is a temporary function that will be deleted soon."""
+    assert spot_is_fun()
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
