9256dec6e63cd0488292af4fe75ca2aa68c1cb5b
Aidan Curtis <curtisa@mit.edu>
2023-03-15 17:33:22 -0400

    updates

--- src/perception/segmentation.py
+++
@@ -1,5 +0,0 @@
-from structures.image import RGBImage
-
-def scene_segment_image(rgbd:RGBImage) -> RGBImage:
-    from src.perception.vision_utils.semseg.interface import get_semantic_labels
-    return get_semantic_labels(rgbd.rgb)
