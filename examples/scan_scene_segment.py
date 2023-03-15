15d6db9afeb79d2d554a3ac00b2b59bec20dfb0c
Aidan Curtis <curtisa@mit.edu>
2023-03-15 15:52:26 -0400

    more examples

--- examples/scan_scene_segment.py
+++ examples/scan_scene_segment.py
@@ -2,22 +2,23 @@
 from controllers.startup import setup_robot
 from controllers.arm import scan_room
 from perception.conversion import rgbds_to_pointcloud
+from perception.segmentation import scene_segment_image
 import open3d as o3d
 
 if __name__ == "__main__":
     """
-        Scan the scene using only the gripper camera,
-        segment the scene with an image scene segmentation network,
-        project the segmentations into a 3d pointcloud,
-        and visualize the segmented pointcloud
+        Scan the scene using only the gripper 
+        camera and visualize the pointcloud in open3d
     """
 
     robot_client = setup_robot()
     rgbds = scan_room(robot_client, num_images=20)
-    
-    pointcloud = rgbds_to_pointcloud(rgbds)
+    segmented_rgbs = [scene_segment_image(rgbd) for rgbd in rgbds]
+    pointcloud = rgbds_to_pointcloud(segmented_rgbs)
+
     point_cloud = o3d.geometry.PointCloud()
     point_cloud.points = o3d.utility.Vector3dVector(pointcloud.points_xyz)
     point_cloud.colors = o3d.utility.Vector3dVector(pointcloud.points_rgb)
 
     o3d.visualization.draw_geometries([point_cloud])
+    
